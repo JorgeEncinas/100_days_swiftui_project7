9d5f9ea4f61fd1a8572b0e3e6cd6782a4303fce9
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-10 17:43:27 -0700

    First lesson

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
-struct User {
+// First we had this, but since we need to share, we will change it into a class instead.
+//struct User {
+//    var firstName = "Bilbo"
+//    var lastName = "Baggins"
+//}
+
+// This one doesn't work with `@State` though
+// that's because with `struct`s, each time we modify it, we actually create
+// a NEW INSTANCE of struct.
+// But with `class`es, the value is modified directly.
+@Observable //Fixes the @State issue.
+class User {
     var firstName = "Bilbo"
     var lastName = "Baggins"
 }
+
+// Classes don't need the `mutating` keyword for functions that modify variables,
+// unlike `struct`s.
+// @State does NOT monitor the State variables.
 
 struct ContentView: View {
     @State private var user = User()
