c24265d64362c49bcb7c2c09b1a2b6676f0f228d
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-11 16:09:38 -0700

    edit a list of things

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -42,8 +42,7 @@ struct SecondViewWithEnvironment : View {
     }
 }
 
-//Showing a second sheet
-struct ContentView: View {
+struct FirstViewWithEnvironment : View {
     @State private var showingSheet = false
     
     var body: some View {
@@ -54,6 +53,57 @@ struct ContentView: View {
                 name: "Johnathan"
             )
         }
+    }
+}
+
+struct DeleteRowsView : View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+    
+    var body : some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id:\.self) { num in
+                        Text("Row \(num)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                //List(numbers, id:\.self) {
+                //    Text("(Row \($0)")
+                //}
+                //While this second `List` View works,
+                //  the `onDelete` modifier ONLY EXISTS ON ForEach
+                
+                //  To make onDelete work
+                //  we need to implement a method that will receive
+                //  a SINGLE parameters of type `IndexSet`
+                
+                //  This is a bit like a Set of Integers
+                //  except it's SORTED
+                //  and it's just telling use the positions
+                //  of ALL the items in the ForEach
+                //  that should be removed.
+                Button("Add number \(currentNumber)") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }.toolbar {
+                EditButton()
+            }
+        }
+    }
+}
+
+//Showing a second sheet
+struct ContentView: View {
+
+    var body : some View {
+        Text("Hello World")
     }
 }
 
