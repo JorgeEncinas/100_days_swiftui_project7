dfc569225121ea10be3cffbadb46ad55ef8f17c9
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-10 17:36:54 -0700

    first commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Jorge Encinas on 7/10/25.
+//
+
+import SwiftUI
+
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jorge Encinas on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
